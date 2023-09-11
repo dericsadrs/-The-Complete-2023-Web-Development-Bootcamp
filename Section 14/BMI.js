8360c5f26e667fa730a3272499112bf583d7b7a2
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-09-11 22:53:14 +0800

    Progression 9/11/23

---
+++ Section 14/BMI.js
@@ -0,0 +1,16 @@
+
+
+var height = 187;
+var weight = 72;
+
+
+
+function getBMI(height, weight) {
+    return weight / (height **2);
+ }
+
+ var output = getBMI(height,weight);
+
+ console.log(output)
+
+ 
