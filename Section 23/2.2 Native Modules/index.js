03999faa8053526efbf8481c1129f3af4cddd5cb
dericthegamerpoints <145327255+dericthegamerpoints@users.noreply.github.com>
2023-11-05 11:12:45 +0800

    Progression 11/5/23

---
+++ Section 23/2.2 Native Modules/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+
+// fs.writeFile("message.txt", "Hello, world!",
+// (err) => {
+//     if(err) throw err;
+//     else console.log("The file has been successfully written");
+// }
+// );
+
+fs.readFile("message.txt","utf-8",(err, data) => {
+    if (err) throw err;
+    else {
+        console.log(data);
+    }
+})
