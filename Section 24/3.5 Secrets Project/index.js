928e5879e5cf2220b720c3ecb77e5c577d13d20e
dericsadrs <drccsadrs@gmail.com>
2023-11-12 15:54:31 +0800

    Change respond to redirect

--- Section 24/3.5 Secrets Project/index.js
+++ Section 24/3.5 Secrets Project/index.js
@@ -29,8 +29,7 @@ app.post("/check", (req, res) => {
   if (userIsAuthorised) {
     res.sendFile(__dirname + "/public/secret.html");
   } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
+    res.redirect(__dirname + "/public/index.html");
   }
 });
 
