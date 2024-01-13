a616d49ab2a6939547bf686e506736cfa3462d78
Deric <drccsadrs@gmail.com>
2024-01-13 14:18:16 +0800

    add react template starter - code

---
+++ Section 36/my-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+ReactDOM.render(
+  <div>
+    <h1>
+      My Favourite Foods
+    </h1>
+    <ul>
+      <li> Pizza </li>
+      <li> Pizza </li>
+      <li> Pizza </li>
+  
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
